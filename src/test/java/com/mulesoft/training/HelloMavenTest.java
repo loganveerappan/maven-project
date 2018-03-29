3f437905194b278b163c3af4429dbd52cca8defe
Logan Veerappan <Logan.Veerappan@ssp-worldwide.com>
2018-03-29 11:31:10 +1100

    Updated derby dependency

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -1,6 +1,5 @@
 package com.mulesoft.training;
 
-import org.junit.Test;
 import org.mule.tck.junit4.FunctionalTestCase;
 
 public class HelloMavenTest extends FunctionalTestCase {
@@ -8,10 +7,5 @@ public class HelloMavenTest extends FunctionalTestCase {
 	@Override
 	protected String getConfigFile() {
 		return "maven-project.xml";
-	}
-
-	//@Test
-	public void mavenFlowReturnsHelloMaven() throws Exception {
-		runFlowAndExpect("mavenFlow", "Hello Maven");
 	}
 }
